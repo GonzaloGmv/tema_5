e96def4e4c70184a9a41501213204cc1627acc71
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2022-12-09 16:15:49 +0000

    commit

---
+++ diccionarios.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+from collections import OrderedDict
+
+d = defaultdict(int)
+d["uno"] = 1
+print(d["uno"])
+
+n = OrderedDict()
+n['uno'] = 'one'
+print(n['uno'])
